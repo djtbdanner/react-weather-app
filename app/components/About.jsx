faa2537ccf8f51cf51dfc8fdd44c6cec316f618c
- <djtbdanner@yahoo.com>
2017-11-08 18:29:48 -0600

    more styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,13 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This was me learning to use React.</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react"> React</a></li>
+        <li><a href="https://openweathermap.org">openWeatherMap<a> This is the API for the weather calls.</li>
+      </ul>
+
     </div>
 
   )
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,7 @@
 var React = require('react');
+var {
+  Link
+} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function() {
@@ -9,9 +12,20 @@ var React = require('react');
 // });
 
 var Examples = (props) => {
-  return (<div>
-    <h3>Examples</h3>
-  </div>)
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Adel">Adel, IA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Des Moines">Des Moines, IA</Link>
+        </li>
+      </ol>
+    </div>
+  )
 };
 
 module.exports = Examples;
